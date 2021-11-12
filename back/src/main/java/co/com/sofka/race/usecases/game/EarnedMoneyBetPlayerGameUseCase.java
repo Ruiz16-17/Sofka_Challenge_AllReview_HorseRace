93485ba478602f23b5786fa35107a32e12b9bfcd
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-11-11 20:24:15 -0500

    Fix earnedbet

--- back/src/main/java/co/com/sofka/race/usecases/game/EarnedMoneyBetPlayerGameUseCase.java
+++ back/src/main/java/co/com/sofka/race/usecases/game/EarnedMoneyBetPlayerGameUseCase.java
@@ -3,6 +3,7 @@ package co.com.sofka.race.usecases.game;
 import co.com.sofka.race.model.GameDTO;
 import co.com.sofka.race.usecases.player.GetPlayerUseCase;
 import co.com.sofka.race.usecases.player.UpdatePlayerUseCase;
+import co.com.sofka.race.usecases.track.GetTrackUseCase;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
@@ -15,22 +16,28 @@ public class EarnedMoneyBetPlayerGameUseCase {
     private final GetGameUseCase getGameUseCase;
     private final UpdatePlayerUseCase updatePlayerUseCase;
     private final GetPlayerUseCase getPlayerUseCase;
+    private final GetTrackUseCase getTrackUseCase;
 
-    public EarnedMoneyBetPlayerGameUseCase(GetGameUseCase getGameUseCase, UpdatePlayerUseCase updatePlayerUseCase, GetPlayerUseCase getPlayerUseCase) {
+    public EarnedMoneyBetPlayerGameUseCase(GetGameUseCase getGameUseCase, UpdatePlayerUseCase updatePlayerUseCase, GetPlayerUseCase getPlayerUseCase, GetTrackUseCase getTrackUseCase) {
         this.getGameUseCase = getGameUseCase;
         this.updatePlayerUseCase = updatePlayerUseCase;
         this.getPlayerUseCase = getPlayerUseCase;
+        this.getTrackUseCase = getTrackUseCase;
     }
 
     public Mono<String> apply(GameDTO gameDTO) {
         return getGameUseCase.apply(gameDTO.getId())
                 .flatMap(
                         foundGameDTO -> getPlayerUseCase.apply(foundGameDTO.getIdUser())
-                                .flatMap(playerDTO -> {
-                                    playerDTO.setMoney(playerDTO.getMoney()+foundGameDTO.getMoneyBet());
-                                    return updatePlayerUseCase.apply(playerDTO)
-                                            .map(s -> playerDTO.getMoney().toString());
-                                })
+                                .flatMap(playerDTO ->
+
+                                        getTrackUseCase.apply(foundGameDTO.getIdTrack())
+                                                .flatMap(trackDTO -> {
+                                                            playerDTO.setMoney(playerDTO.getMoney() + (foundGameDTO.getMoneyBet()*trackDTO.getLanes().length));
+                                                            return updatePlayerUseCase.apply(playerDTO);
+                                                        }
+                                                ).map(s -> playerDTO.getMoney().toString())
+                                )
                 );
     }
 
--- back/src/test/java/co/com/sofka/race/usecases/game/EarnedMoneyBetPlayerGameUseCaseTest.java
+++ back/src/test/java/co/com/sofka/race/usecases/game/EarnedMoneyBetPlayerGameUseCaseTest.java
@@ -2,10 +2,13 @@ package co.com.sofka.race.usecases.game;
 
 import co.com.sofka.race.collections.Game;
 import co.com.sofka.race.collections.Player;
+import co.com.sofka.race.collections.Track;
 import co.com.sofka.race.model.GameDTO;
 import co.com.sofka.race.model.PlayerDTO;
+import co.com.sofka.race.model.TrackDTO;
 import co.com.sofka.race.reposioties.GameRepository;
 import co.com.sofka.race.reposioties.PlayerRepository;
+import co.com.sofka.race.reposioties.TrackRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -25,6 +28,9 @@ class EarnedMoneyBetPlayerGameUseCaseTest {
 
     @MockBean
     GameRepository gameRepository;
+
+    @MockBean
+    TrackRepository trackRepository;
 
     @Test
     void earnedMoneyBetPlayerGameUseCaseTest(){
@@ -48,17 +54,32 @@ class EarnedMoneyBetPlayerGameUseCaseTest {
         player.setMoney(playerDTO.getMoney());
         playerDTO.setMoneyBet(1L);
 
+        int[] lanes = {1,2,3};
+        int[][] racecourse = {{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}};
+        var trackDTO = new TrackDTO("1","trackName",1,lanes,racecourse,false,-1);
+        var track = new Track();
+
+        track.setId(trackDTO.getId());
+        track.setName(trackDTO.getName());
+        track.setKm(trackDTO.getKm());
+        track.setLanes(trackDTO.getLanes());
+        track.setRacecourse(trackDTO.getRacecourse());
+        track.setCompleted(trackDTO.isCompleted());
+        track.setLaneWinner(trackDTO.getLaneWinner());
+
+
         Mockito.when(gameRepository.save(Mockito.any(Game.class))).thenReturn(Mono.just(game));
         Mockito.when(playerRepository.save(Mockito.any(Player.class))).thenReturn(Mono.just(player));
+        Mockito.when(trackRepository.findById(Mockito.any(String.class))).thenReturn(Mono.just(track));
         Mockito.when(gameRepository.findById(Mockito.any(String.class))).thenReturn(Mono.just(game));
         Mockito.when(playerRepository.findById(Mockito.any(String.class))).thenReturn(Mono.just(player));
 
         var resultMono = earnedMoneyBetPlayerGameUseCase.apply(gameDTO);
         var result = resultMono.block();
 
-        Long moneyAfterEarnBet = player.getMoney() + playerDTO.getMoneyBet();
+        Long moneyAfterEarnBet = player.getMoney() + (playerDTO.getMoneyBet()*lanes.length);
 
-        assert result != null;
+            assert result != null;
 
         Assertions.assertEquals(Long.parseLong(result), moneyAfterEarnBet);
 
